8ee8e91c4282ebf97d8ee6fb68bc1027ae7de545
GuoErr <43460012+DaraBoth@users.noreply.github.com>
2023-10-29 10:11:50 +0700

    init

---
+++ src/assets/index.js
@@ -0,0 +1,85 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import googleadsense from "./work/googleadsense.png";
+import rupp from "./work/rupp.png";
+import phsatech from "./work/phsatech.jpg";
+import acc from "./work/acc.jpg";
+import makerlight from "./work/makerlight.png";
+import hrd from "./work/hrd.jpg";
+import kosign from "./work/KOSIGN.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import nextjs from "./nextjs.png";
+import profile from "./profile.jpg";
+import accpremiumwrap from "./accpremiumwrap.png";
+import kshrdregi from "./kshrdregi.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  nextjs,
+  profile,
+  accpremiumwrap,
+  kshrdregi,
+  googleadsense,
+  rupp,
+  phsatech,
+  acc,
+  makerlight,
+  hrd,
+  kosign,
+};
---
+++ src/components/About.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className="xs:w-[250px] w-full">
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
+        <img
+          src={icon}
+          alt="web-development"
+          className="w-16 h-16 object-contain"
+        />
+
+        <h3 className="text-white text-[20px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        Introducing DaraBoth, a multi-talented web developer with expertise
+        in a diverse range of technologies. With a strong background in
+        AngularJS, Next.js, React.js, JavaScript, and jQuery, DaraBoth crafts
+        dynamic and interactive front-end experiences. On the back-end, their
+        skills extend to Express.js, Spring Boot with JPA, JSP, and Flask with
+        Python, enabling them to build robust and scalable server-side
+        applications. With proficiency in both PostgreSQL and MySQL, DaraBoth
+        leverages their database knowledge to ensure optimal data management and
+        performance. With their extensive technical repertoire, DaraBoth is
+        poised to tackle any web development challenge with finesse and
+        innovation.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
