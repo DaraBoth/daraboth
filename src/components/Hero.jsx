c63a3d7de0b85ee33ae9984dad110e1d23051803
GuoErr <43460012+DaraBoth@users.noreply.github.com>
2023-10-29 11:27:20 +0700

    Update Hero.jsx
    Fix color

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,13 +10,13 @@ const Hero = () => {
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#FDE6FA1]" />
+          <div className="w-5 h-5 rounded-full bg-[#fde6fa1]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#FDE6FA1]">DaraBoth</span>
+            Hi, I'm <span className="text-[#fde6fa1]">DaraBoth</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I am a skilled software engineer <br className="sm:block hidden" />I
