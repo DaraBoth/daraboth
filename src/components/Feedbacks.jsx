e016fc7504a79b41906fdb77b028fe81d4f09151
daraboth <daraboth0331@gmail.com>
2024-09-23 14:25:28 +0900

    khos style tic

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -66,15 +66,12 @@ const Feedbacks = ({ refreshToggle }) => {
 
   return (
     <div
-      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
+      className={`xl:mt-12  flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
     >
-      <motion.div
-        variants={slideIn("left", "tween", 0.2, 1)}
-        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
-      >
+      <motion.div className="flex-[0.75] bg-black-100 p-8 rounded-2xl">
         <p className={styles.sectionSubText}>What others say</p>
         <h2 className={styles.sectionHeadText}>Testimonials.</h2>
-        <div className="-mt-20 pb-14 ${styles.paddingX} flex flex-wrap gap-7">
+        <div className="mt-20 pb-14 ${styles.paddingX} flex flex-wrap gap-7">
           {testimonials.length > 0 ? (
             <>
               <Marquee pauseOnHover className="[--duration:20s]">
