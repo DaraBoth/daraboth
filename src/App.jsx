0ee20e94576aff8f0626308b2c6213cdef4ec2d7
GuoErr <43460012+DaraBoth@users.noreply.github.com>
2025-08-10 18:31:25 +0900

    Remove console.log from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import HomePage from "./pages/homePage";
 import TestimonialFormWrapper from "@/components/TestimonialFormWrapper";
 
 const App = () => {
-  console.log("I'm here");
   return (
     <BrowserRouter>
       <Routes>
